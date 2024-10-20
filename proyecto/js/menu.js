f202a67d3a59168d76340bf94e98325ff23ae66b
IssaMs46 <isams2004.1@gmail.com>
2024-10-20 14:15:33 -0500

    listo el menu hamburguesa

---
+++ proyecto/js/menu.js
@@ -0,0 +1,15 @@
+// Esperar a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+  // Seleccionar el ícono del menú hamburguesa
+  const menuIcon = document.querySelector('.menu-icon');
+  
+  // Seleccionar el menú de navegación
+  const navMenu = document.querySelector('#nav-menu');
+  
+  // Agregar un evento de click al ícono del menú
+  menuIcon.addEventListener('click', function() {
+      // Alternar la clase 'active' para mostrar u ocultar el menú
+      navMenu.classList.toggle('active');
+  });
+});
+
