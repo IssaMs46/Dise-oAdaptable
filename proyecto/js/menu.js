c9f796379ab1b07786d46b38170e467209f586f8
Pipe2303 <piperoldanrestrepo@hotmail.com>
2024-10-22 09:58:39 -0500

    Wpp Js and Sparkles

--- proyecto/js/menu.js
+++ proyecto/js/menu.js
@@ -18,6 +18,63 @@ document.getElementById("menu-toggle").addEventListener("click", function() {
   navMenu.classList.toggle("active");
 });
 
+function sendWhatsAppMessage() {
+  // Define el número de teléfono
+  const phoneNumber = '+573008995356';
+  // Define el mensaje que quieres enviar
+  const message = 'Hola TalentDisplay! estoy interesado en adquirir un producto ';
+  // Crea la URL de WhatsApp
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  // Abre WhatsApp en una nueva pestaña
+  window.open(whatsappUrl, '_blank');
+}
+
+
+
+
+const container = document.getElementById('effect-container');
+
+// Crear un destello
+function createSparkle(x, y) {
+  const sparkle = document.createElement('div');
+  sparkle.classList.add('sparkle');
+  sparkle.style.left = `${x}px`;
+  sparkle.style.top = `${y}px`;
+  container.appendChild(sparkle);
+
+  // Eliminar el destello después de la animación
+  setTimeout(() => {
+    sparkle.remove();
+  }, 1000);
+}
+
+// Manejar el movimiento del mouse
+let mouseMoving = false;
+let lastX, lastY;
+
+document.addEventListener('mousemove', (e) => {
+  if (!mouseMoving) {
+    mouseMoving = true;
+    requestAnimationFrame(() => {
+      createSparkle(e.pageX, e.pageY);
+      mouseMoving = false;
+    });
+  }
+});
+
+// Crear efecto de explosión al hacer clic
+document.addEventListener('click', (e) => {
+  const explosion = document.createElement('div');
+  explosion.classList.add('sparkle', 'click-effect');
+  explosion.style.left = `${e.pageX}px`;
+  explosion.style.top = `${e.pageY}px`;
+  container.appendChild(explosion);
+
+  // Eliminar la explosión después de la animación
+  setTimeout(() => {
+    explosion.remove();
+  }, 500);
+});
 
 
 
